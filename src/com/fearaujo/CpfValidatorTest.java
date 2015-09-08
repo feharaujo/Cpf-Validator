e5761956caea78f9dcde82822857bc0cd3aeb298
Felipe Trova de Araujo <fetrova@gmail.com>
2015-09-08 15:47:24 -0300

    Fix

--- src/com/fearaujo/CpfValidatorTest.java
+++ src/com/fearaujo/CpfValidatorTest.java
@@ -9,8 +9,10 @@ public class CpfValidatorTest {
 	
 	@Test
 	public void cpfTest(){
-		
+		// invalid cpfs
 		// cpfs invalidos
+		assertEquals(false, CpfValidator.isCPF(null));
+		assertEquals(false, CpfValidator.isCPF(""));
 		assertEquals(false, CpfValidator.isCPF("0"));
 		assertEquals(false, CpfValidator.isCPF("00000000000"));
 		assertEquals(false, CpfValidator.isCPF("11111111111"));
@@ -24,8 +26,23 @@ public class CpfValidatorTest {
 		assertEquals(false, CpfValidator.isCPF("321.324.545-44"));
 		assertEquals(false, CpfValidator.isCPF("456.567.767-57"));
 		
+		assertEquals(false, CpfValidator.isCPF("071.721.522-98"));
+		assertEquals(false, CpfValidator.isCPF("847.781.803-72"));
+		assertEquals(false, CpfValidator.isCPF("471.382.531-09"));
+		assertEquals(false, CpfValidator.isCPF("434.896.155-73"));
+		assertEquals(false, CpfValidator.isCPF("179.947.946-37"));
+		assertEquals(false, CpfValidator.isCPF("328.179.426-20"));
+		assertEquals(false, CpfValidator.isCPF("916.326.148-04"));
+		assertEquals(false, CpfValidator.isCPF("761.859.339-69"));
+		
+		assertEquals(false, CpfValidator.isCPF("916.326.148-**"));
+		assertEquals(false, CpfValidator.isCPF("761.859.339-##"));
+		
+		
+		// valid cpfs
+		// is not allowed special characters and masks
 		// cpfs validos
-		// nao e permetido caracteres especiais
+		// nao e permitido caracteres especiais e mascaras
 		assertEquals(true, CpfValidator.isCPF("07172152298"));
 		assertEquals(true, CpfValidator.isCPF("84778180372"));
 		assertEquals(true, CpfValidator.isCPF("47138253109"));
